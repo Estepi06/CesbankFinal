4c1a3bb45834ac3fdc583d6567ade0358652fa15
¨Arianna¨ <zambranoarianna21@gmail.com>
2026-06-02 18:47:57 -0500

    Revert "Ajustando ultimos cambios"
    This reverts commit 2719e8ec3b22ac515d06a13b6de0bf111d955cb0.

--- src/main/java/cesde/persistence/repository/TransaccionRepositoryMemory.java
+++ src/main/java/cesde/persistence/repository/TransaccionRepositoryMemory.java
@@ -1,7 +1,7 @@
 package cesde.persistence.repository;
 
 import cesde.domain.Transaccion;
-import cesde.port.output.TransaccionPersistencePort;
+import cesde.service.portoutput.TransaccionPersistencePort;
 
 import java.util.ArrayList;
 import java.util.HashMap;
